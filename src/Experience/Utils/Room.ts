e5de342de7b8248f28b507f2548cd77e6328c820
John <bioj@hotmail.fr>
2022-12-09 19:06:49 +0900

    port 8000

--- src/Experience/Utils/Room.ts
+++ src/Experience/Utils/Room.ts
@@ -10,7 +10,7 @@ export default class Room {
   players: { [key: string]: Player } = {}
 
   // State
-  socket = io("wss://52.201.230.114:443")
+  socket = io("https://52.201.230.114:8000")
 
   constructor() {
     Room._instance = this
